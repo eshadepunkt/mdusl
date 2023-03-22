6527dabc97332f0b9767717ca7c0f4cc163ba269
Martin Freiberger <s80411@htw-dresden.de>
2023-03-22 14:23:16 +0100

    added task 3 and 4

--- prak 01/übung01.r
+++ prak 01/übung01.r
@@ -42,7 +42,7 @@ covariance_xy <- sum((x - avg_x) * (y - avg_y)) / (length(x) - 1)
 print(covariance_xy)
 
 # d)
-
+# sd() - standard deviation
 correlation_xy <- covariance_xy / (sqrt(variance_x) * sqrt(variance_y))
 print(correlation_xy)
 
@@ -57,3 +57,47 @@ mean_y <- mean(y)
 cov_xy <- cov(x, y)
 
 cor_xy <- cor(x, y)
+
+# Aufgabe 3
+# a)
+
+x1 <- c(-2,4,0.5,-4,1,2,-0.1,5,0.2,5,-0.4)
+
+# b)
+
+arr <- 1:length(x1)
+print(x2 <- x1^(-1) + arr)
+
+# c)
+
+print(x3 <- sort(x2, TRUE))
+
+# d)
+
+print(x4 <- seq(-2, 15, length.out = 11))
+
+# e)
+
+print(x5 <- pmin(x1, x2, x3, x4))
+
+# Aufgabe 4
+
+# a)
+
+seed <- set.seed(321)
+
+print(x1 <- rnorm(100, 4, 4))
+
+# b)
+
+print(summary(x1))
+
+# c)
+
+print ( table( x1 < 0))
+
+# d)
+
+hist(x1, freq = FALSE, col = FALSE)
+
+curve(dnorm(x, mean = 4, sd = 4), add = TRUE, col = "red", lwd = 2)
